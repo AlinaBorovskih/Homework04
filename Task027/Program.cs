0d7650ee787cdddb5e3bea480d1422dd35178d0d
Alina_Borovskih <alinaberseneva@icloud.com>
2022-05-01 16:51:30 +0300

    Add task27

---
+++ Task027/Program.cs
@@ -0,0 +1,10 @@
+﻿
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+int s = 0;
+while (a > 0)
+{
+    s=s+a%10;
+    a=a/10;
+}
+Console.WriteLine($"Сумма равна {s}");
