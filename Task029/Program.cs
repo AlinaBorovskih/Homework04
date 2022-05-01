bfd56f87b4cb00e0a78556ae08d295f6953b1de3
Alina_Borovskih <alinaberseneva@icloud.com>
2022-05-01 16:53:36 +0300

    Add task29

---
+++ Task029/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] mass = new int[8];
+{
+   for (int i = 0; i < mass.Length; i++)
+    {
+       mass[i] = int.Parse(Console.ReadLine());
+    }
+
+for (int i = 0; i < mass.Length; i++ )
+{
+Console.Write($"{mass[i]} ");
+}
+}
