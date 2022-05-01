fbd8fbe1c800409347ca6ccf7dcff0302e79d8c8
Alina_Borovskih <alinaberseneva@icloud.com>
2022-05-01 16:48:49 +0300

    Add task25

---
+++ Task025/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите степень: ");
+int B = int.Parse(Console.ReadLine());
+
+
+for (int i = 0; i <= B; i++)
+{
+   Console.WriteLine($"{A}^{i} = {Math.Pow(A,i)}");
+}
